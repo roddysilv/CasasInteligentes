89cf53c31484ce0cf338265462a348d17575ee13
Leonardo <goliatt@gmail.com>
2021-03-29 16:50:12 -0300

    goliatt seg mar 29 16:50:12 -03 2021

--- residential_reading.py
+++ residential_reading.py
@@ -91,3 +91,36 @@ for x,s in zip(A,r):
 pl.axis('equal');
 
 #%%
+from sklearn.cluster import AffinityPropagation
+af = AffinityPropagation(preference=None).fit(X)
+cluster_centers_indices = af.cluster_centers_indices_
+labels = af.labels_
+
+n_clusters_ = len(cluster_centers_indices)
+
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+plt.close('all')
+plt.figure(1)
+plt.clf()
+
+colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+for k, col in zip(range(n_clusters_), colors):
+    class_members = labels == k
+    cluster_center = A[cluster_centers_indices[k]]
+    plt.plot(A[class_members, 0], A[class_members, 1], col + '.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=6)
+    for x in A[class_members]:
+        plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
+
+for x,s in zip(A,r): 
+    pl.text(x=x[0], y=x[1], s=s, fontsize=8)  
+    
+#pl.axis('equal');
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+
+
+#%%
