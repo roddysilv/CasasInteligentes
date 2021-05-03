3d65107db5768878b8c42a8e5cdf78b8af8a0390
Rodrigo Oliveira <rodrigo.silvoliveira@gmail.com>
2021-05-03 11:56:37 -0300

    Adiciona dados tratados

--- neural/neuraltest.py
+++ neural/neuraltest.py
@@ -20,29 +20,31 @@ from keras.callbacks import EarlyStopping
 import pandas as pd 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 start = time.time()
 
-# t = pd.read_csv("bungalow/Residential_1.csv")
-t = pd.read_csv("teste.csv")
+df = pd.read_csv("special/Residential_9_treated.csv")
+df.pop('weather')
+#t = pd.read_csv("teste.csv")
 
-X = t.drop(['energy_kWh'],axis=1).values
-y = t['energy_kWh'].values
+
+X = df.drop(['energy_kWh','date'],axis=1)
+y = df['energy_kWh']
 
 # X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 
-train_size = int(t.shape[0]*.9)
-X_train, X_test = X[0:train_size], X[train_size:]
-y_train, y_test = y[0:train_size], y[train_size:]
+train_size = int(df.shape[0]*.7)
+dim = X.shape[1]
+X_train, X_test = X[0:train_size].values, X[train_size:].values
+y_train, y_test = y[0:train_size].values, y[train_size:].values
 
 
 model = Sequential()
-model.add(Dense(512, activation="relu"))
-model.add(Dense(128))
-model.add(Dense(32))
-model.add(Dense(8))
-
+model.add(Dense(64, activation="relu", input_dim=dim))
+model.add(Dense(64, activation="relu"))
+model.add(Dense(64, activation="relu"))
 
 model.add(Dense(1))
 
@@ -53,10 +55,8 @@ model.compile(loss='mean_squared_error', optimizer=Adam(lr=1e-3, decay=1e-3 / 20
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=200)
 
 # Fit the model
-# history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10000000, batch_size=100, verbose=2, callbacks=[es])
-history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10000)
-
-# Calculate predictions
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10000000, batch_size=100, verbose=2, callbacks=[es])
+# history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=] Calculate predictions
 PredTestSet = model.predict(X_train)
 PredValSet = model.predict(X_test)
 
@@ -83,17 +83,30 @@ plt.show()
 TestR2Value = r2_score(y_train,TestResults)
 print("Training Set R-Square=", TestR2Value)
 
+print('Tempo decorrido:',str(datetime.timedelta(seconds=(time.time() - start))))
+
+
 #%%
 # PredValSet.sort()
 # y_test.sort()
 
-plt.plot(PredValSet[:50])
-plt.plot(y_test[:50])
+plt.plot(PredValSet[:50],'k--',label='predicted')
+plt.plot(y_test[:50],label='test')
+plt.title('Prediction 50')
+plt.legend()
 plt.show()
 
 #%%
+plt.plot(PredValSet[:100],'k--',label='predicted')
+plt.plot(y_test[:100],label='test')
+plt.title('Prediction 100')
+plt.legend()
+plt.show()
 
-plt.plot(PredValSet[:100])
-plt.plot(y_test[:100])
+#%%
+plt.plot(PredValSet[:500],'k--',label='predicted')
+plt.plot(y_test[:500],label='test')
+plt.title('Prediction 500')
+plt.legend()
+plt.show()
 
-print('Tempo decorrido:',str(datetime.timedelta(seconds=(time.time() - start))))
